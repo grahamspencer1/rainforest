2fc9aa4a0b1c1371c9a7c29976aca83a01e22a78
Valerie Osei <valerie.osei@gmail.com>
2018-08-15 16:37:46 -0400

    created review form

--- rainforest/app/controllers/reviews_controller.rb
+++ rainforest/app/controllers/reviews_controller.rb
@@ -1,12 +1,23 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.new
+    @review.comment = params[:product][:comment]
+    if @review.save
+      redirect_to product_url
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review = Review.find(params[:id])
+    @review.comment = params[:product][:comment]
+    if @review.save
+      redirect_to product_url
+    end
   end
 
   def destroy
