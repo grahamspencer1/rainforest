9db3617abb826a66b9aede8130efcf2913df9d00
Graham Spencer <grahamspence@gmail.com>
2018-08-15 11:33:49 -0400

    filled out controller

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,31 +1,49 @@
 class ProductsController < ApplicationController
 
   def index
-
-  end
-
-  def create
-
-  end
-
-  def new
-
-  end
-
-  def edit
-
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price = params[:product][:price]
+    if @product.save
+      redirect_to products_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
   end
 
   def update
-
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price = params[:product][:price]
+    if @product.save
+      redirect_to product_url(params[:id])
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url
   end
 
 
