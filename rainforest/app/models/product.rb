d4667432e840d0db9af3bd3e02b4294e34c57e71
Graham Spencer <grahamspence@gmail.com>
2018-08-15 15:13:45 -0400

    added dollars to price

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ApplicationRecord
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :price, :numericality => {:greater_than => 0}
+
+  def price_in_dollars
+    price_in_dollars = price.to_f / 100
+    sprintf("$%.2f", price_in_dollars)
+  end
 
 end
